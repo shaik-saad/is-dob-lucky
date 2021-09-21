823ef7c226ba597bdee3c74bf82ddb5cc739e3e0
Shaik Saad <shaiksaadullahshareef@gmail.com>
2021-09-22 02:40:28 +0530

    feat: added app functionality

--- src/App.js
+++ src/App.js
@@ -2,17 +2,80 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [dateOfBirth, setDateOfBirth] = useState("")
+  const [luckyNumber, setLuckyNumber] = useState("")
+  const [message, setMessage] = useState()
+
+  // input
+  function dobChangeHandler(event){
+    setDateOfBirth(event.target.value)
+  }
+
+  function luckyNumberChangeHandler(event){
+    setLuckyNumber(event.target.value)
+  }
+
+  // processing
+  function onClickHandler(){
+    // removing hyphens from the date of birth
+    var dob = dateOfBirth.replaceAll("-", "")
+    var sum = 0
+    // looping over the dob
+    for (let index = 0; index < dob.length; index++) {
+      // adding each number of date of birth
+      sum = sum + Number(dob.charAt(index))
+    }
+    // dividing the total sum of dob by lucky number and checking if remainder is equal to zero
+    // if yes then user's dob is lucky and vice versa
+    // output
+    if(sum%luckyNumber === 0){
+      setMessage(<p id="result-message" style={{"backgroundColor": "rgba(0, 255, 0, 0.5)"}}>"Yay! your Birthday is luckyyy! 🎉"</p>)
+    } else {
+      setMessage(<p id="result-message" style={{"backgroundColor": "rgba(255, 255, 0, 0.5)"}}>"Oops! your birthday is not lucky. 😕"</p>)
+    }
+    setShowReset(true)
+  }
+
+  function resetClickHandler(){
+    setDateOfBirth("")
+    setLuckyNumber("")
+    setMessage("")
+    setShowReset(false)
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Is your Birthday lucky?</h1>
+        <h1>Is your Birthday lucky? 🤔</h1>
       </header>
       <div className="inner-container">
         <label htmlFor="date-of-birth">Date of Birth:</label>
-        <input id="dateof-birth" type="date" />
+        <input 
+          id="dateof-birth" 
+          type="date"
+          onChange={dobChangeHandler}
+          value={dateOfBirth}
+          required
+        />
         <label htmlFor="lucky-number">Lucky Number:</label>
-        <input id="lucky-number" type="number" placeholder="Enter a number"/>
-        <button id="check-btn">Check Number</button>
+        <input 
+          id="lucky-number" 
+          type="number" 
+          placeholder="Enter a number"
+          onChange={luckyNumberChangeHandler}
+          value={luckyNumber}
+          required
+        />
+        { showReset ? 
+            <button id="reset-btn" onClick={() => resetClickHandler()}>Reset</button>
+          :
+            <button 
+              id="check-btn"
+              onClick={() => onClickHandler()}
+            >
+              Check Number
+            </button>
+            }
+        <div>{message}</div>
       </div>
       <footer>
         <p>If you loved❤️ this app, make sure to follow me on</p>
