7fcd1642ab56f718549bb66be41ff9964ffba42c
Shaik Saad <shaiksaadullahshareef@gmail.com>
2021-09-22 00:05:56 +0530

    feat: added structure of the website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Is your Birthday lucky?</h1>
       </header>
+      <div className="inner-container">
+        <label htmlFor="date-of-birth">Date of Birth:</label>
+        <input id="dateof-birth" type="date" />
+        <label htmlFor="lucky-number">Lucky Number:</label>
+        <input id="lucky-number" type="number" placeholder="Enter a number"/>
+        <button id="check-btn">Check Number</button>
+      </div>
+      <footer>
+        <p>If you loved❤️ this app, make sure to follow me on</p>
+        <ul id="non-bullet-list">
+          <li className="list-item">
+            <a className="link" href="https://www.linkedin.com/in/shaik-saad">
+                <i className="fab fa-linkedin"></i>
+            </a>
+          </li>
+          <li className="list-item">
+            <a className="link" href="https://twitter.com/shaiksaadullah">
+                <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li className="list-item">
+            <a className="link" href="https://github.com/shaik-saad">
+                <i className="fab fa-github"></i>
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
